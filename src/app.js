55df689947fec0edc98158c7192af4b16e737cbe
erico <ecocaval@hotmail.com>
2023-01-25 21:36:42 -0300

    Adicionando CRUD para os carrinhos dos usuários

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import cors from "cors"
 import userRouter from "./routes/userRoutes.js"
 import tokenRouter from "./routes/tokenRoutes.js"
 import loginRouter from "./routes/loginRoutes.js"
+import cartRouter from "./routes/cartsRoutes.js"
 
 const PORT = 5000
 
@@ -17,5 +18,6 @@ app.use(express.json())
 app.use(userRouter)
 app.use(tokenRouter)
 app.use(loginRouter)
+app.use(cartRouter)
 
 app.listen(PORT, () => console.log(`Server Initialized. Port: ${PORT} `))
